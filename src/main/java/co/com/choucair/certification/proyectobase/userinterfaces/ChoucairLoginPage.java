d9320785bead7238209b41ad11116a4e00d09940
Hroyero <hroyero22@gmail.com>
2021-06-03 15:56:29 -0500

    se eliminaron los extends PageObject y se cambio el .gitignore
    Se eliminaron los extends PageObject, solo se dejo en la anotación @DefaultUrl del archivo ChoucairAcademyPage.java
    Y se modifico el archivo .gitignore, para no cargar información innecesaria en el repositorio

--- src/main/java/co/com/choucair/certification/proyectobase/userinterfaces/ChoucairLoginPage.java
+++ src/main/java/co/com/choucair/certification/proyectobase/userinterfaces/ChoucairLoginPage.java
@@ -1,11 +1,11 @@
 package co.com.choucair.certification.proyectobase.userinterfaces;
 
-import net.serenitybdd.core.pages.PageObject;
+
 import net.serenitybdd.screenplay.targets.Target;
 import org.openqa.selenium.By;
 
 
-public class ChoucairLoginPage  extends PageObject {
+public class ChoucairLoginPage   {
     public static final Target LOGIN_BUTTON = Target.the("Button that shows us the from to login").located(By.xpath("//header/div[1]/nav[1]/ul[2]/li[1]/a[1]"));
     public static final Target INPUT_USER = Target.the("Where do we write the user").located(By.xpath("//input[@id='username']"));
     public static final Target INPUT_PASSWORD = Target.the("where do we write the password").located(By.xpath("//input[@id='password']"));
